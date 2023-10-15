497c340b2541b35ed592f3b9d8c4a821b19d2f66
Gordon-T <93834879+Gordon-T@users.noreply.github.com>
2023-10-15 15:31:33 -0400

    Incomplete authentication

---
+++ server.py
@@ -0,0 +1,69 @@
+from flask import *
+from utils.database import *
+
+app = Flask(__name__, static_folder='public')
+
+@app.after_request
+def addHeader(response): #Applies no sniff header
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
+
+#Base URL request
+#Check if user is logged in, if true redirect to app, else send login page
+@app.route('/')
+def root():
+    existingCookie = request.cookies.get('auth_token')
+    if existingCookie:
+        token, username = authenticate(None, None, existingCookie, False)
+        if token != False: #User is logged in, send app html
+            return render_template('app.html')
+        else: #Not logged in or expired cookie, send login page
+            return render_template('login.html')
+    else: #Not logged in, send login page
+        return render_template('login.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    body = request.get_json()
+    username = body.get("username")
+    password = body.get("password")
+    result = registerDB(username, password)
+    if result == False:
+        response = app.response_class(
+            response="Username is taken, try a different username",
+            status=200,
+            mimetype='text/plain'
+        )
+        return response
+    elif result == True:
+        response = app.response_class(
+            response="Registration successful",
+            status=200,
+            mimetype='text/plain'
+        )
+        return response
+
+@app.route('/login', methods=['POST'])
+def login():
+    body = request.get_json()
+    username = body.get("username")
+    password = body.get("password")
+    token, username = authenticate(username, password, None, True)
+    if token == False:
+        response = app.response_class(
+            response = "Incorrect username or password",
+            status = 200,
+            mimetype = 'text/plain'
+        )
+        return response
+    elif token != False: #Set cookie
+        response = app.response_class(
+            response = "Login Successful! Welcome ",
+            status = 200,
+            mimetype='text/plain'
+        )
+        response.set_cookie("auth_token", token, max_age=3600) #TODO: HttpOnly header as well?
+        return response
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=8080)
