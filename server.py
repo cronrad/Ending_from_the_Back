65061a56aa379e16be7b9325298e9da95234d2cf
aryan-kum <aryankum@buffalo.edu>
2023-10-22 22:43:41 -0400

    HTML Escaped Posts

--- server.py
+++ server.py
@@ -92,8 +92,8 @@ def new_post():
             return response
         else:
             #Adds the post to database
-            title = body.get("title")
-            description = body.get("description")
+            title = HTMLescaper(body.get("title"))
+            description = HTMLescaper(body.get("description"))
             #Find the next ID available
             cur = postDB.find()
             id = 0
